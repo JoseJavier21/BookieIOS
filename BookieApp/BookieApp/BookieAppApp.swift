daf2e17e69b5268435d6bbc1139cf3e1896ca368
JoseJavier21 <javier.rojo@escuelaestech.es>
2024-03-08 19:19:37 +0100

    trabajo bueno

---
+++ BookieApp/BookieApp/BookieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookieAppApp.swift
+//  BookieApp
+//
+//  Created by dam2 on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BookieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
