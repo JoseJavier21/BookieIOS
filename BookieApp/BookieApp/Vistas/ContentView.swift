b0af1dd8a2cfda0bcf907ce0e38dd3ed966c50d8
JoseJavier21 <javier.rojo@escuelaestech.es>
2024-03-11 19:04:35 +0100

    carpetas

---
+++ BookieApp/BookieApp/Vistas/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  BookieApp
+//
+//  Created by dam2 on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
